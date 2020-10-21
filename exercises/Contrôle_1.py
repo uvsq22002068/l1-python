070e19135d04d46217b50b4d1bb4ebacd1fb7160
Pierre-Emmanuel SCREVE <pierre-emmanuel.screve@ens.uvsq.fr>
2020-10-21 18:39:03 +0200

    fin de contrôle

---
+++ exercises/Contrôle_1.py
@@ -0,0 +1,20 @@
+#a = int(input("Entrez un nombre de votre choix"))
+#if a >= 25:
+#    print("Gagné")
+#else:
+#    print("Dommage")
+
+#n = 2
+#for i in range(n):
+#    print("a"*(n-1) + "b")
+
+import random
+x = random.randint(1, 10)
+y = random.randint(1, 10)
+i = 1
+while x != y:
+    x = random.randint(1, 10)
+    y = random.randint(1, 10)
+    i += 1
+print(x, y)
+print(i)
