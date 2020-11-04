638d2d03de59bc453f0c1e5169333755d4a72ea1
Pierre-Emmanuel SCREVE <pierre-emmanuel.screve@ens.uvsq.fr>
2020-11-04 16:02:51 +0100

    brouillon controle

--- exercises/Contrôle_1.py
+++ exercises/Contrôle_1.py
@@ -8,13 +8,13 @@
 #for i in range(n):
 #    print("a"*(n-1) + "b")
 
-import random
-x = random.randint(1, 10)
-y = random.randint(1, 10)
-i = 1
-while x != y:
-    x = random.randint(1, 10)
-    y = random.randint(1, 10)
-    i += 1
-print(x, y)
-print(i)
+#import random
+#x = random.randint(1, 10)
+#y = random.randint(1, 10)
+#i = 1
+#while x != y:
+#    x = random.randint(1, 10)
+#    y = random.randint(1, 10)
+#    i += 1
+#print(x, y)
+#print(i)
